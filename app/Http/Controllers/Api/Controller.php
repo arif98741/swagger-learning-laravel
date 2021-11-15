4a18e8a7e9137baaf2afea759ef9e9cc7e0c4690
Ariful <arif98741@gmail.com>
2021-11-15 23:49:31 +0600

    init

---
+++ app/Http/Controllers/Api/Controller.php
@@ -0,0 +1,29 @@
+<?php
+
+class Controller
+{
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="Take Care Application Api Documentation",
+     *      description="This is api documentation for getting data and also updating data to server using several endpoints. ",
+     *      @OA\Contact(
+     *          email="arif98741@gmail.com"
+     *      ),
+     *      @OA\License(
+     *          name="Apache 2.0",
+     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url="http://127.0.0.1:8000/api/v1",
+     *      description="Demo API Server"
+     * )
+     *
+     * @OA\Tag(
+     *     name="Projects",
+     *     description="API Endpoints of Projects"
+     * )
+     */
+}
---
+++ app/Http/Controllers/Api/V1/Admin/ProjectsApiController.php
@@ -0,0 +1,268 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+use App\Http\Controllers\Controller;
+
+class ProjectsApiController extends Controller
+{
+    /**
+     * @OA\Get(
+     *      path="/projects",
+     *      operationId="getProjectsList",
+     *      tags={"Projects"},
+     *      summary="Get list of projects",
+     *      description="Returns list of projects",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/ProjectResource")
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
+     */
+    public function index()
+    {
+        abort_if(Gate::denies('project_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return new ProjectResource(Project::with(['author'])->get());
+    }
+
+    /**
+     * @OA\Post(
+     *      path="/projects",
+     *      operationId="storeProject",
+     *      tags={"Projects"},
+     *      summary="Store new project",
+     *      description="Returns project data",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/StoreProjectRequest")
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Project")
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     * )
+     */
+    public function store(StoreProjectRequest $request)
+    {
+        $project = Project::create($request->all());
+
+        return (new ProjectResource($project))
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    /**
+     * @OA\Get(
+     *      path="/projects/{id}",
+     *      operationId="getProjectById",
+     *      tags={"Projects"},
+     *      summary="Get project information",
+     *      description="Returns project data",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Project id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Project")
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     * )
+     */
+    public function show(Project $project)
+    {
+        abort_if(Gate::denies('project_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        return new ProjectResource($project->load(['author']));
+    }
+
+    /**
+     * @OA\Put(
+     *      path="/projects/{id}",
+     *      operationId="updateProject",
+     *      tags={"Projects"},
+     *      summary="Update existing project",
+     *      description="Returns updated project data",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Project id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *      ),
+     *      @OA\Response(
+     *          response=202,
+     *          description="Successful operation",
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource Not Found"
+     *      )
+     * )
+     */
+    public function update(UpdateProjectRequest $request, Project $project)
+    {
+        $project->update($request->all());
+
+        return (new ProjectResource($project))
+            ->response()
+            ->setStatusCode(Response::HTTP_ACCEPTED);
+    }
+
+    /**
+     * @OA\Delete(
+     *      path="/projects/{id}",
+     *      operationId="deleteProject",
+     *      tags={"Projects"},
+     *      summary="Delete existing project",
+     *      description="Deletes a record and returns no content",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Project id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=204,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource Not Found"
+     *      )
+     * )
+     */
+    public function destroy(Project $project)
+    {
+        abort_if(Gate::denies('project_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        $project->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @OA\Get(
+     *      path="/project/test/{id}/{dataid}",
+     *      operationId="getTestingProject",
+     *      tags={"Projects"},
+     *      summary="Get project testing information",
+     *      description="Data for getting projects",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Project id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *     @OA\Parameter(
+     *          name="dataid",
+     *          description="Data id",
+     *          required=false,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Project")
+     *       ),
+     *     @OA\Response(
+     *          response=201,
+     *          description="Not Completed",
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     * )
+     */
+    public function testapi()
+    {
+        return response()->json([
+            'name' => 'Ariful Islam',
+            'address' => 'Elenga , Tangail'
+        ]);
+    }
+}
